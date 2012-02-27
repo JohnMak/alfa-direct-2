99158757481981a42ab2ebbda8affba76e279d68
JohnMak <makeev.john@gmail.com>
2012-02-27 13:40:58 +0700

    processing all files in category

--- engine.py
+++ engine.py
@@ -1,24 +1,33 @@
-﻿import test
+﻿import os
+import test
 import data
 
-fileInName='data.txt'
-data.init(fileInName)
+comingDataDir = 'coming data/'
+proccesedDataDir = 'processed data/'
+outputImagesDir = 'output images/'
 
-#timeStart='20111003_103000'
-#timeEnd='20111202_183000'
+files = os.listdir(comingDataDir)
 
-timeStart='20111003_103000'
-timeEnd='20120224_183000'
+for file in files:
+    fileInName = comingDataDir + file
+    print fileInName
+    data.init(fileInName)
+
+    #timeStart='20111003_103000'
+    #timeEnd='20111202_183000'
+
+    timeStart='20111003_103000'
+    timeEnd='20120224_183000'
 
 
-max_dif=-1000
+    max_dif=-1000
+    fileToSave = proccesedDataDir + file[:-4] + '.csv'
+    fo = open(fileToSave, 'w+')
 
-fo = open('res.csv', 'w+')
-
-for i in range(1,201):
-	for j in range(i+1,201):
-		value = test.strat_reverse_calc(i, j, timeStart, timeEnd)
-		fo.write(str(i)+','+str(j)+','+str(value)+'\n')
-		max_dif=max(max_dif,value)
-	print str(i)+','+str(j)+' = '+str(max_dif)
-fo.close()
+    for i in range(1,201):
+        for j in range(i+1,201):
+            value = test.strat_reverse_calc(i, j, timeStart, timeEnd)
+            fo.write(str(i)+','+str(j)+','+str(value)+'\n')
+            max_dif=max(max_dif,value)
+        print str(i)+','+str(j)+' = '+str(max_dif)
+    fo.close()
